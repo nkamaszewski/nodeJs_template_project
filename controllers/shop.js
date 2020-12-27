ee8798ff70123e726c98dc5fa81ce9b01ccdd151
Norbert Kamaszewski <norbert.kamaszewski@gmail.com>
2020-12-27 21:11:46 +0100

    added product detail

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      pageTitle: product.title,
+      path: '/products',
+      product,
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/index', {
